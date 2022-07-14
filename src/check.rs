840c03cdecf20f80136a970172df64c2244861ae
ivanjermakov <ivanjermakov1@gmail.com>
2022-07-14 22:50:32 +0200

    Clap cli

---
+++ src/check.rs
@@ -0,0 +1,3 @@
+pub fn check(config_path: &String) {
+    println!("Check: {}", config_path);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+mod check;
+
+use clap::{Parser, Subcommand, ValueHint};
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+#[clap(propagate_version = true)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    #[clap(about = "Check config validity")]
+    Check(CheckArgs),
+}
+
+#[derive(Parser, Debug)]
+struct CheckArgs {
+    #[clap(short, long, value_parser, value_hint = ValueHint::FilePath, default_value = "./confs.yml")]
+    config_path: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::Check(args) => {
+            check::check(&args.config_path);
+        }
+    }
 }

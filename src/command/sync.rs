8ee6498606b52a7bebbc8bae52c187b1f2128bca
ivanjermakov <ivanjermakov1@gmail.com>
2025-03-10 19:07:41 +0100

    Sync: proper dest path

--- src/command/sync.rs
+++ src/command/sync.rs
@@ -1,31 +1,48 @@
-use std::fs::{copy, create_dir_all};
+use std::fs::{copy, create_dir, create_dir_all};
 use std::path::Path;
 
-use log::{debug, info};
+use log::{debug, info, warn};
 
 use crate::command::check::check_fatal;
 use crate::config::{pretty_item, Config};
 use crate::fs::{expand, item_matches};
 
 pub fn sync(config: &Config) {
-    let root = expand(&config.root);
-    let root_path = Path::new(&root);
+    let root_path = Path::new(&config.root);
     let mut copy_count = 0;
 
     check_fatal(config).and_then(|_| create_dir_all(root_path)).unwrap();
 
     config.items.iter().for_each(|item| {
+        let item_root = expand(&item.root);
+        let item_root_path = Path::new(&item_root);
         let matches = item_matches(item);
-        let dest = root_path.join(&item.dir);
-        create_dir_all(&dest).unwrap();
+        let dest_dir = root_path.join(&item.dir);
+        create_dir_all(&dest_dir).unwrap();
         matches.iter().for_each(|f| {
-            debug!("{} Comparing files: {:?} | {:?}", pretty_item(item), f, &dest);
-            if dest.exists() {
-                debug!("\tDestination file exists");
+            if !f.exists() {
+                debug!("Source file does not exist");
+                return;
             }
-            // TODO: strip prefix
-            info!("{} Copying file: {:?} -> {:?}", pretty_item(item), f, &dest);
-            copy(f, &dest).unwrap();
+
+            let dest = &dest_dir.join(f.strip_prefix(item_root_path).unwrap());
+            if dest.exists() {
+                debug!("Destination file exists");
+            }
+
+            if f.is_dir() {
+                info!("{} Creating dir: {:?}", pretty_item(item), dest);
+                match create_dir(dest) {
+                    Ok(_) => {}
+                    Err(e) => warn!("mkdir error: {e}"),
+                }
+            } else {
+                info!("{} Copying file: {:?} -> {:?}", pretty_item(item), f, dest);
+                match copy(f, dest) {
+                    Ok(_) => {}
+                    Err(e) => warn!("copy error: {e}"),
+                }
+            }
             copy_count += 1;
         })
     });
